4699beed7fe991a6d8c5d5ffe2ab78ec6e38c05e
Andrew Kim <38521355+nn98@users.noreply.github.com>
2026-07-10 07:33:16 +0900

    chore: scaffold Spring Boot project

---
+++ src/main/java/com/nextstep/NextstepApplication.java
@@ -0,0 +1,15 @@
+package com.nextstep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+@ConfigurationPropertiesScan
+public class NextstepApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NextstepApplication.class, args);
+    }
+}
