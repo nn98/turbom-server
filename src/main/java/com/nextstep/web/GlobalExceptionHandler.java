0a9e07e5e69dbeb1ac6f04182d830756a6126885
wonsang.choi <wonsang.choi@e4net.net>
2026-07-16 17:47:18 +0900

    fix: log unexpected exceptions in GlobalExceptionHandler

--- src/main/java/com/nextstep/web/GlobalExceptionHandler.java
+++ src/main/java/com/nextstep/web/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import com.nextstep.domain.exception.InvalidQueryException;
 import com.nextstep.domain.exception.SiteNotFoundException;
 import com.nextstep.domain.exception.UnitNotFoundException;
 import com.nextstep.web.dto.ApiDtos.ErrorResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(InvalidQueryException.class)
     public ResponseEntity<ErrorResponse> handleInvalidQuery(InvalidQueryException e) {
@@ -29,6 +33,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleUnexpected(Exception e) {
+        log.error("Unhandled exception", e);
         return ResponseEntity.internalServerError().body(new ErrorResponse("INTERNAL_ERROR", "서버 오류가 발생했습니다."));
     }
 }
