6d75df93fe7c75e8b3a8fb234de4a5f4b9733e1a
wonsang.choi <wonsang.choi@e4net.net>
2026-07-22 23:32:56 +0900

    chore: allow localhost:8080-8088 and turbom-client.vercel.app in CORS

--- src/main/java/com/nextstep/web/CorsConfig.java
+++ src/main/java/com/nextstep/web/CorsConfig.java
@@ -10,7 +10,19 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:8080", "https://turbom.vercel.app", "https://ter-view.vercel.app")
+                .allowedOrigins(
+                        "http://localhost:8080",
+                        "http://localhost:8081",
+                        "http://localhost:8082",
+                        "http://localhost:8083",
+                        "http://localhost:8084",
+                        "http://localhost:8085",
+                        "http://localhost:8086",
+                        "http://localhost:8087",
+                        "http://localhost:8088",
+                        "https://turbom.vercel.app",
+                        "https://ter-view.vercel.app",
+                        "https://turbom-client.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
